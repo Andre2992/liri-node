5ffd03b55d13d45f84128d969588ed6424b962b6
andre2992 <andrebrown2929@gmail.com>
2018-06-18 22:06:35 -0400

    updates

--- liri.js
+++ liri.js
@@ -1,2 +1,34 @@
 require("dotenv").config();
-var r
+var request = require('request');
+var keys = require("./keys");
+var spotify = require('spotify_api');
+var twitter = require ('twitter_api');
+var fs = require('fs');
+var spotify = new spotify(keys.spotify);
+var client = new twitter (keys.twitter);
+// varibles created for the command and user input
+// the 
+var command = process.argv[2];
+var userInput = process.argv[3];
+
+
+switch (command) {
+        case `my-tweets`:
+        myTweets();
+        break;
+
+        case `spotify-this-song`:
+        spotifyThisSong(userInput);
+        break;
+
+        case `movie-this`:
+        movieLookup(userInput);
+        break;
+
+        case `do-what-it-says`:
+        doWhatItSays();
+        break;
+
+    default:
+        break;
+}
